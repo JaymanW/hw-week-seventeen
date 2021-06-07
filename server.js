0311a28723e3f9868a797b050f33cc4b945b095e
Jayden W <jaymanw2000@gmail.com>
2021-06-07 15:04:17 -0500

    removed env

--- server.js
+++ server.js
@@ -6,12 +6,16 @@ require('dotenv').config();
 const port = 3000;
 
 // const uri = `mongodb+srv://sliderDB:${process.env.mongo_password}@slider.opswz.mongodb.net/slider?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://dbUser:${process.env.mongo_password}@cluster0.3wf9l.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// mongoose.connect(process.env.)
+mongoose.connect(uri, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+})
 
 app.listen(process.env.PORT || port, () => {
     console.log(`Server successfully running on port: ${process.env.PORT || port}`)
